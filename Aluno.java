3731476d82b27bbe408dab1ab7863e72b6dc0494
ANTONIO  JUNIOR <junior.fragalevi95@gmail.com>
2025-12-05 18:11:35 -0300

    Enhance Aluno.java with comments for clarity
    Added comments for clarity and encapsulation.

--- Aluno.java
+++ Aluno.java
@@ -1,14 +1,15 @@
 public class Aluno {
+// Atributos privados pra garantir o encapsulamento
     private String nome;
     private String email;
     private int matricula;
-    
+ // Construtor padrão pra já criar o aluno com o básico necessário     
     public Aluno(String nome, String email) {
         this.nome = nome;
         this.email = email;
     }
     
-    // Getters e Setters
+ // Getters e Setters
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
     
@@ -16,8 +17,10 @@ public class Aluno {
     public void setEmail(String email) { this.email = email; }
     
     public int getMatricula() { return matricula; }
+ // A matrícula vai ser setada pelo sistema depois, não no construtor
     public void setMatricula(int matricula) { this.matricula = matricula; }
-    
+
+// Sobrescrita do toString pra facilitar a visualização no console depois.
     @Override
     public String toString() {
         return "Matrícula: " + matricula + " | Nome: " + nome + " | Email: " + email;
